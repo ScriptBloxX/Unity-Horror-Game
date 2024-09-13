7b5ee0bf5af697790f7d2eb99aa7ae618e4c280e
9hodev <siaskkh4884@gmail.com>
2024-09-13 14:15:42 +0700

    update brain system

--- Assets/Script/CheckingScript/global_value.cs
+++ Assets/Script/CheckingScript/global_value.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class global_value : MonoBehaviour
 {
@@ -6,13 +8,30 @@ public class global_value : MonoBehaviour
     public int PlayerConsciousness;
     public InteractBox target_1;
     private bool fuse1Trigger;
-
+    void Start()
+    {
+        StartCoroutine(RegenConsciousness());    
+    }
     void Update(){
         if(target_1.Enable){
             lightOff = false;
             if(!fuse1Trigger){
                 fuse1Trigger = true;
-                GameObject.FindGameObjectWithTag("global").GetComponent<global_value>().PlayerConsciousness -= 50;
+                Debug.Log("On Trigger");
+                PlayerConsciousness -= 50;
+            }
+        }
+    }
+    IEnumerator RegenConsciousness(){
+        while (true){
+            yield return new WaitForSeconds(2f);
+            if(PlayerConsciousness >49 && PlayerConsciousness<100){
+                PlayerConsciousness++;
+            }else if(PlayerConsciousness>0 && PlayerConsciousness<30){
+                PlayerConsciousness--;
+            }else if(PlayerConsciousness<=0){
+                PlayerConsciousness = 100;
+                SceneManager.LoadScene("OutdoorsScene");
             }
         }
     }
